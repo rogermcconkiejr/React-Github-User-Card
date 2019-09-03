fd995f5af25f423b77b32b612f3302dc396da2db
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-03 13:59:27 -0600

    axios call implemented, state set

--- githubcard/src/App.js
+++ githubcard/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  state = {
+    cards: []
+  };
+
+  componentDidMount(){
+    axios
+      .get('https://api.github.com/users/rogermcconkiejr')
+      .then((response) => {
+        console.log('data:', response)
+        this.setState({cards: response.data})
+      })
+      .catch(err => console.log('error'));
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
+  }
 }
 
 export default App;
